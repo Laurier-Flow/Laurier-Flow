0c199af917a2bd782a8273fd536d8f812297eab2
Muhammad Mujtaba <mmujtaba832@gmail.com>
2024-01-04 17:20:30 -0500

    got started on searchbar component.

---
+++ app/search/SearchBar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+function HandleSearch(searchQuery: string) {
+    console.log(searchQuery);
+}
+
+
+export default function SearchBar({ placeholder }: { placeholder: string }) {
+  return (
+    <div className="">
+        <input type="text" placeholder={placeholder} onChange={(e) => HandleSearch(e.target.value)} size={placeholder.length}></input>
+    </div>
+    );
+}
