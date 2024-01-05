6f288565cef5b169673a4eb0dce8214610251400
Muhammad Mujtaba <mmujtaba832@gmail.com>
2024-01-04 19:05:21 -0500

    got basic PoC query working for the SearchBar Component.

--- app/search/SearchBar.tsx
+++ app/search/SearchBar.tsx
@@ -1,7 +1,29 @@
 "use client";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
 
-function HandleSearch(searchQuery: string) {
-  console.log(searchQuery);
+async function HandleSearch(searchQuery: string) {
+  // const [courses, setCourses] = useState<any[] | null>(null)
+  const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL
+    ? process.env.NEXT_PUBLIC_SUPABASE_URL
+    : "";
+  const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    : "";
+  const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+  const { data, error } = await supabase.from("courses").select("*").limit(10);
+  console.log(data);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await supabase.from('courses').select('*').limit(10);
+  //     setCourses(data)
+  //   }
+  //   getData()
+  // }, [])
+
+  // return <pre>{JSON.stringify(courses, null, 2)}</pre>
 }
 
 export default function SearchBar({ placeholder }: { placeholder: string }) {
