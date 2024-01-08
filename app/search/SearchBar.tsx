11507928724ebf4493500dc852dda4dc5fee7e13
Muhammad Mujtaba <mmujtaba832@gmail.com>
2024-01-07 20:17:01 -0500

    instead of returning objects out of the handler function, i locally scoped to the search bar component itself. cleaned up logging aswell

--- app/search/SearchBar.tsx
+++ app/search/SearchBar.tsx
@@ -1,40 +1,34 @@
 "use client";
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/database.types";
-
-async function HandleSearch(searchQuery: string) {
-  // const [courses, setCourses] = useState<any[] | null>(null)
-  const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL
-    ? process.env.NEXT_PUBLIC_SUPABASE_URL
-    : "";
-  const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    : "";
-  const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
-
-  const { data, error } = await supabase.from("courses").select("*").limit(10);
-  console.log(data);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const { data } = await supabase.from('courses').select('*').limit(10);
-  //     setCourses(data)
-  //   }
-  //   getData()
-  // }, [])
-
-  // return <pre>{JSON.stringify(courses, null, 2)}</pre>
-}
+import { createClient } from "@/utils/supabase/client";
 
 export default function SearchBar({ placeholder }: { placeholder: string }) {
-  return (
-    <div className="">
-      <input
-        type="text"
-        placeholder={placeholder}
-        onChange={(e) => HandleSearch(e.target.value)}
-        size={placeholder.length}
-      ></input>
-    </div>
-  );
+	async function handleSearch(searchQuery: string) {
+		const supabase = createClient();
+		const { data: courseResults, error: courseErr } = await supabase
+			.from("courses")
+			.select()
+			.ilike("course_code", `%${searchQuery}%`);
+	
+		const { data: profResults, error: profErr } = await supabase
+			.from("instructors")
+			.select()
+			.ilike("instructor_name", `%${searchQuery}%`);
+		if (courseErr) {
+			console.log(courseErr);
+		}
+		if (profErr) {
+			console.log(profErr);
+		}
+	}
+
+	return (
+		<div className="">
+			<input
+				type="text"
+				placeholder={placeholder}
+				onChange={(e) => handleSearch(e.target.value)}
+				size={placeholder.length}
+			></input>
+		</div>
+	);
 }
