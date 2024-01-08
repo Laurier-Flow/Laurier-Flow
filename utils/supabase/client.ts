8b5f4bff1c8d041205bb2113f93631bc4dccc77a
Muhammad Mujtaba <mmujtaba832@gmail.com>
2024-01-08 10:50:24 -0500

    fixed ts config file

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,5 +1,5 @@
 import { createBrowserClient } from '@supabase/ssr'
-import { Database } from '@/database.types'
+import { Database } from '@/utils/supabase/database.types'
 
 export const createClient = () =>
   createBrowserClient<Database>(
