a4a14ef205585619070e536a505f9163820e349f
Muhammad Mujtaba <mmujtaba832@gmail.com>
2024-02-18 13:51:10 -0500

    added footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className='flex flex-row w-screen border-t border-t-foreground/10 h-max'>
+        FOOTER PLACEHOLDER
+      </div>
+    </footer>
+  )
+}
