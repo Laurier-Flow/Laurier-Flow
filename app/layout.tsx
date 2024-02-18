dc9fa88195ef606b437cb7cf1c978baf456fabff
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-18 15:38:10 -0500

    fix centering on courses page

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main className='flex w-screen flex-grow'>
+          <main className='justify-center flex w-screen flex-grow'>
             {children}</main>
           <Footer />
         </ThemeProvider>
