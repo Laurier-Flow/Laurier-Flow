4ef578b3458060d36ab6d1870e5cdfff29b6455a
SohamNagi <nagisoham@gmail.com>
2024-02-23 00:17:49 -0500

    Cleaned up component and added static images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,31 +23,37 @@ export default function About() {
         choose great courses and avoid not so great ones.
       </p>
       <h2 className="text-xl py-2 font-semibold">Our History</h2>
+      <p className="pb-2">
+        The most handsome brown boys from the double degree decided to
+        collaborate and bless the WLU community - "Soham Nagi"
+      </p>
       <h2 className="text-xl py-2 font-semibold">Meet The Team</h2>
-      <TeamMember
-        name="Faizaan Qureshi"
-        role="Heiseinberg"
-        imageUrl="https://media.licdn.com/dms/image/D5603AQFyfypgQBmhfg/profile-displayphoto-shrink_200_200/0/1693374520689?e=1714003200&v=beta&t=oDNIja1nwBRq1JhSfzwKEVflwcz8kqHzf1MnGxA_qU0"
-        linkedinUrl="https://www.linkedin.com/in/faizaan-qureshi/"
-      />
-      <TeamMember
-        name="Muhammad Mujtaba"
-        role="Software"
-        imageUrl="https://media.licdn.com/dms/image/D5603AQHe4-dd8dHOuA/profile-displayphoto-shrink_200_200/0/1679012694874?e=1714003200&v=beta&t=O94YxSvNJEFybvZAZq-r5i0lqPRD1XViR5FCSvmDqhw"
-        linkedinUrl="https://www.linkedin.com/in/mhmmd-m/"
-      />
-      <TeamMember
-        name="Abdullah Shahid"
-        role="Software"
-        imageUrl="https://media.licdn.com/dms/image/D5635AQGyT8kmV6G2sQ/profile-framedphoto-shrink_800_800/0/1676559863827?e=1709269200&v=beta&t=rsvISe9ealuLHwClVwav7TbyHeMgZW9ddvLmBnuyc64"
-        linkedinUrl="https://www.linkedin.com/in/abdullahshahid247/"
-      />
-      <TeamMember
-        name="Soham Nagi"
-        role="Software"
-        imageUrl="https://media.licdn.com/dms/image/D5603AQGm58eD6354Mg/profile-displayphoto-shrink_200_200/0/1680819421625?e=1714003200&v=beta&t=odnVt9GuGnqWjzsGd5N8bEk7QbL8sTOtTVXI9HFuWrY"
-        linkedinUrl="https://www.linkedin.com/in/sohamnagi/"
-      />
+      <div>
+        <TeamMember
+          name="Faizaan Qureshi"
+          role="Heiseinberg"
+          imageUrl="https://media.licdn.com/dms/image/D5603AQFyfypgQBmhfg/profile-displayphoto-shrink_200_200/0/1693374520689?e=1714003200&v=beta&t=oDNIja1nwBRq1JhSfzwKEVflwcz8kqHzf1MnGxA_qU0"
+          linkedinUrl="https://www.linkedin.com/in/faizaan-qureshi/"
+        />
+        <TeamMember
+          name="Muhammad Mujtaba"
+          role="Software"
+          imageUrl="https://media.licdn.com/dms/image/D5603AQHe4-dd8dHOuA/profile-displayphoto-shrink_200_200/0/1679012694874?e=1714003200&v=beta&t=O94YxSvNJEFybvZAZq-r5i0lqPRD1XViR5FCSvmDqhw"
+          linkedinUrl="https://www.linkedin.com/in/mhmmd-m/"
+        />
+        <TeamMember
+          name="Abdullah Shahid"
+          role="Software"
+          imageUrl="/teampics/D95268AF-DD70-4D92-9F88-DC436949BBB6.jpg"
+          linkedinUrl="https://www.linkedin.com/in/abdullahshahid247/"
+        />
+        <TeamMember
+          name="Soham Nagi"
+          role="Software"
+          imageUrl="https://media.licdn.com/dms/image/D5603AQGm58eD6354Mg/profile-displayphoto-shrink_200_200/0/1680819421625?e=1714003200&v=beta&t=odnVt9GuGnqWjzsGd5N8bEk7QbL8sTOtTVXI9HFuWrY"
+          linkedinUrl="https://www.linkedin.com/in/sohamnagi/"
+        />
+      </div>
     </div>
   );
 }
--- components/TeamMember.tsx
+++ components/TeamMember.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // Define a type for the props that the TeamMember component will accept
 type TeamMemberProps = {
@@ -9,19 +9,37 @@ type TeamMemberProps = {
 };
 
 // Define the TeamMember component with props destructuring
-const TeamMember: React.FC<TeamMemberProps> = ({ name, role, imageUrl, linkedinUrl }) => {
+const TeamMember: React.FC<TeamMemberProps> = ({
+  name,
+  role,
+  imageUrl,
+  linkedinUrl,
+}) => {
   return (
-    <div className="max-w-xs mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="py-4 rounded-xl overflow-hidden md:max-w-2xl">
       <div className="md:flex">
         <div className="md:flex-shrink-0">
-          <img className="h-48 w-full object-cover md:h-full md:w-48" src={imageUrl} alt={`Profile of ${name}`} />
+          <img
+            className="h-48 rounded-full w-full scale-75 object-cover md:h-full md:w-48"
+            src={imageUrl}
+            alt={`Profile of ${name}`}
+          />
         </div>
         <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{role}</div>
-          <div className="block mt-1 text-lg leading-tight font-medium text-black">{name}</div>
-          <p className="mt-2 text-gray-500">Enjoys food, music, and coding.</p>
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            {role}
+          </div>
+          <div className="block mt-1 text-lg leading-tight font-medium ">
+            {name}
+          </div>
+          <p className="mt-2">Enjoys food, music, and coding.</p>
           <div className="flex mt-4">
-            <a href={linkedinUrl} className="text-indigo-500 hover:text-indigo-600">LinkedIn</a>
+            <a
+              href={linkedinUrl}
+              className="text-indigo-500 hover:text-indigo-600"
+            >
+              LinkedIn
+            </a>
           </div>
         </div>
       </div>
