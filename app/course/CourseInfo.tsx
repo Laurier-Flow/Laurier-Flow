eefbd1733877e055e85c66d4ff17de86c6529b09
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-23 08:00:58 -0500

    add more type definitions

--- app/course/CourseInfo.tsx
+++ app/course/CourseInfo.tsx
@@ -66,7 +66,11 @@ async function getCourseDescription() {
   }
 }
 
-async function CourseInfo({ supabase }: { supabase: any }) {
+async function CourseInfo({
+  supabase
+}: {
+  supabase: SupabaseClient<any, "public", any>
+}) {
   const prerequisites: any = await getPrerequisites();
   const courseDescription: any = await getCourseDescription();
   const courseData: any = await getCourseData(supabase);
