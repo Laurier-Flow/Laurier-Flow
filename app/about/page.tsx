4a4eed5a869d5b294dd9471177c4b4dee5c3f888
SohamNagi <nagisoham@gmail.com>
2024-02-22 22:36:35 -0500

    base about us page finished

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,14 @@ import TeamMember from "@/components/TeamMember"
 
 export default function About(){
     return (
-        <div>
-            <h1 className="text-3xl text-center font-bold underline">About WLU Flow</h1>
-            <h2>Welcome to WLU Flow!</h2>
-            <p>Flow is a course planning website for Wlifried Laurier University students. You can find everything from courses and professors to prerequisites here. Our mission is simple: to empower Laurier students through uncensored and unfiltered course and professor reviews. Had a bad prof? We want to know. Had a great one? Tell us as well. Found a course completely and utterly useless? Leave a review. Your experiences help us help 100s of students like you every month choose great courses and avoid not so great ones.</p>
-            <h2>Our History</h2>
-            <h2>Meet The Team</h2>
+        <div className="w-8/12 bg-center" >
+            <h1 className="text-3xl py-5 text-center font-bold underline">About WLU Flow</h1>
+            <h2 className="text-xl py-2 font-semibold">Welcome to WLU Flow!</h2>
+                <p className="pb-2">Flow is a course planning website for University of Waterloo students. You can find everything from courses and professors to prerequisites here.</p>
+                <p className="pb-2">Our mission is simple: to empower UWaterloo students through uncensored and unfiltered course and professor reviews.</p>
+                <p className="pb-2">Had a bad prof? We want to know. Had a great one? Tell us as well. Found a course completely and utterly useless? Leave a review. Your experiences help us help more than 30,000 students like you every month choose great courses and avoid not so great ones.</p>
+            <h2 className="text-xl py-2 font-semibold">Our History</h2>
+            <h2 className="text-xl py-2 font-semibold">Meet The Team</h2>
                 <TeamMember />
                 <TeamMember />
                 <TeamMember />
