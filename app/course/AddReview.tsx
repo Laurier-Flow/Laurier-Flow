64cc1014ae4dc68f1e07ad13d3050ebf7d177359
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-23 07:56:42 -0500

    change add a review button colour to purple

--- app/course/AddReview.tsx
+++ app/course/AddReview.tsx
@@ -7,7 +7,7 @@ function AddReview({
     return (
         <div className="flex flex-col p-4">
             <h1 className="pb-4 text-xl">What do you think of {courseName}?</h1>
-            <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            <button type="button" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-primary text-white hover:bg-indigo-400 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                 Add your review
             </button>
         </div>
