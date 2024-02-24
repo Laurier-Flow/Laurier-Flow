e4cc8b89bec7c685a773316df9f47fd095afd076
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-23 23:25:44 -0500

    add instructor info component

--- app/instructor/page.tsx
+++ app/instructor/page.tsx
@@ -1,12 +1,17 @@
 import Loading from "@/components/Loading";
 import { Suspense } from "react";
 import InstructorInfo from "./InstructorInfo";
+import { cookies } from 'next/headers';
+import { createClient } from '@/utils/supabase/server';
 
 function InstructorPage() {
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
     return (
         <div className="flex flex-col justify-evenly w-full bg-slate-50 dark:bg-slate-950 lg:max-w-6xl lg:border-x-2 dark:lg:border-slate-900 lg:pl-6 lg:pr-6">
             <Suspense fallback={<Loading />}>
-                <InstructorInfo />
+                <InstructorInfo supabase={supabase} />
             </Suspense>
         </div>
     )
