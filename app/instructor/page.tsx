a1362276a6897ff7f312090a0c92a8e4ef6224d9
Faizaan Qureshi <qfaizaan@gmail.com>
2024-02-24 01:06:23 -0500

    review page addition

--- app/instructor/page.tsx
+++ app/instructor/page.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import InstructorInfo from "./InstructorInfo";
 import { cookies } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
+import InstructorReviews from "./InstructorReviews";
 
 function InstructorPage() {
     const cookieStore = cookies()
@@ -12,6 +13,9 @@ function InstructorPage() {
         <div className="flex flex-col justify-evenly w-full bg-slate-50 dark:bg-slate-950 lg:max-w-6xl lg:border-x-2 dark:lg:border-slate-900 lg:pl-6 lg:pr-6">
             <Suspense fallback={<Loading />}>
                 <InstructorInfo supabase={supabase} />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
+                <InstructorReviews supabase={supabase} />
             </Suspense>
         </div>
     )
