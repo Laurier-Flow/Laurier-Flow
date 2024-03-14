70b80a0b627fbf05a5e4779e8d6a43e9fadae82f
SohamNagi <nagisoham@gmail.com>
2024-03-14 16:15:19 -0400

    Created responsive teammember comp with icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,13 @@ import { Suspense } from "react";
 
 export default function About() {
   return (
-    <Suspense fallback={<div className="w-full h-full"><Spinner /></div>}>
+    <Suspense
+      fallback={
+        <div className="w-full h-full">
+          <Spinner />
+        </div>
+      }
+    >
       <div className="card">
         <h1 className="text-3xl py-3 text-center font-bold">About WLU Flow</h1>
         <hr className="border-gray-300 dark:border-gray-800"></hr>
@@ -15,14 +21,14 @@ export default function About() {
           here.
         </p>
         <p className="pb-2">
-          Our mission is simple: to empower UWaterloo students through uncensored
-          and unfiltered course and professor reviews.
+          Our mission is simple: to empower UWaterloo students through
+          uncensored and unfiltered course and professor reviews.
         </p>
         <p className="pb-2">
-          Had a bad prof? We want to know. Had a great one? Tell us as well. Found
-          a course completely and utterly useless? Leave a review. Your
-          experiences help us help more than 30,000 students like you every month
-          choose great courses and avoid not so great ones.
+          Had a bad prof? We want to know. Had a great one? Tell us as well.
+          Found a course completely and utterly useless? Leave a review. Your
+          experiences help us help more than 30,000 students like you every
+          month choose great courses and avoid not so great ones.
         </p>
         <h2 className="text-xl py-2 font-semibold">Our History</h2>
         <p className="pb-2">
@@ -33,33 +39,38 @@ export default function About() {
         <div>
           <TeamMember
             name="Faizaan Qureshi"
-            role="Heisenberg"
+            role="Heisenberg - Software Engineering 2014"
             imageUrl="https://media.licdn.com/dms/image/D5603AQFyfypgQBmhfg/profile-displayphoto-shrink_200_200/0/1693374520689?e=1714003200&v=beta&t=oDNIja1nwBRq1JhSfzwKEVflwcz8kqHzf1MnGxA_qU0"
             linkedinUrl="https://www.linkedin.com/in/faizaan-qureshi/"
+            igUrl="https://www.instagram.com/faizaanqureshi_"
           />
           <TeamMember
             name="Muhammad Mujtaba"
-            role="Software"
+            role="Founder — Software Engineering 2014"
             imageUrl="https://media.licdn.com/dms/image/D5603AQHe4-dd8dHOuA/profile-displayphoto-shrink_200_200/0/1679012694874?e=1714003200&v=beta&t=O94YxSvNJEFybvZAZq-r5i0lqPRD1XViR5FCSvmDqhw"
             linkedinUrl="https://www.linkedin.com/in/mhmmd-m/"
+            igUrl="https://www.instagram.com/_.mh.mm.d._"
           />
           <TeamMember
             name="Abdullah Shahid"
-            role="Software"
-            imageUrl="/teampics/D95268AF-DD70-4D92-9F88-DC436949BBB6.jpg"
+            role="Founder — Software Engineering 2014"
+            imageUrl="https://media.licdn.com/dms/image/D5635AQGyT8kmV6G2sQ/profile-framedphoto-shrink_400_400/0/1676559863827?e=1711051200&v=beta&t=Nj-D38Scomvmd0hBp9KsWEfcrbt0NuqTz4UR0fbBeJk"
             linkedinUrl="https://www.linkedin.com/in/abdullahshahid247/"
+            igUrl="https://www.instagram.com/abdshd.247"
           />
           <TeamMember
             name="Soham Nagi"
-            role="Software"
+            role="Founder — Software Engineering 2014"
             imageUrl="https://media.licdn.com/dms/image/D5603AQGm58eD6354Mg/profile-displayphoto-shrink_200_200/0/1680819421625?e=1714003200&v=beta&t=odnVt9GuGnqWjzsGd5N8bEk7QbL8sTOtTVXI9HFuWrY"
             linkedinUrl="https://www.linkedin.com/in/sohamnagi/"
+            igUrl="https://www.instagram.com/soham.nagi"
           />
           <TeamMember
             name="Shahrukh Qureshi"
-            role="Software"
+            role="Founder — Software Engineering 2014"
             imageUrl="https://media.licdn.com/dms/image/D5603AQG3D_-qrcORWQ/profile-displayphoto-shrink_200_200/0/1679183158797?e=1714003200&v=beta&t=LW9hoEV5KC24p5hR4-SV04vaG3f7ZU5ny7dywg6VN8c"
             linkedinUrl="https://www.linkedin.com/in/qureshishahrukh/"
+            igUrl="https://www.instagram.com/shahruk._h"
           />
         </div>
       </div>
--- components/TeamMember.tsx
+++ components/TeamMember.tsx
@@ -6,6 +6,7 @@ type TeamMemberProps = {
   role: string;
   imageUrl: string;
   linkedinUrl: string;
+  igUrl: string;
 };
 
 // Define the TeamMember component with props destructuring
@@ -14,34 +15,46 @@ const TeamMember: React.FC<TeamMemberProps> = ({
   role,
   imageUrl,
   linkedinUrl,
+  igUrl,
 }) => {
   return (
-    <div className="py-4 rounded-xl overflow-hidden md:max-w-2xl">
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img
-            className="h-48 rounded-full w-full scale-75 object-cover md:h-full md:w-48"
-            src={imageUrl}
-            alt={`Profile of ${name}`}
-          />
-        </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            {role}
-          </div>
-          <div className="block mt-1 text-lg leading-tight font-medium ">
-            {name}
-          </div>
-          <p className="mt-2">Enjoys food, music, and coding.</p>
-          <div className="flex mt-4">
-            <a
-              href={linkedinUrl}
-              className="text-indigo-500 hover:text-indigo-600"
+    <div className="flex items-center p-4">
+      <img
+        className="rounded-full w-32 h-32 m:w-48 m:h-48 object-cover"
+        src={imageUrl}
+        alt={`Team Member Profile of ${name}`}
+      />
+      <div className="pl-4">
+        <div className="flex items-center">
+          <a href={linkedinUrl} className="">
+            <div className="flex items-center">
+              <p className="tracking-wide text-lg pr-2 font-semibold">{name}</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 text-blue-600 hover:text-blue-800 h-4"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+            </div>
+          </a>
+          <a href={igUrl} className="text-pink-600 pl-2 hover:text-pink-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 24 24"
             >
-              LinkedIn
-            </a>
-          </div>
+              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+            </svg>
+          </a>
         </div>
+        <p className=" font-extrabold">{role}</p>
+        <p>
+          Derrek designed the new UW Flow and occasionally writes frontend code
+          - he is interested in the intersection of software and design.
+        </p>
       </div>
     </div>
   );
