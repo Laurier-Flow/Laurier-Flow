b2157016a48c478a3185d35ad5c72f7746d02c88
Faizaan Qureshi <qfaizaan@gmail.com>
2024-04-15 23:08:04 -0400

    add dark and light mode to home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import SearchBar from './search/SearchBar'
 import { BackgroundGradientAnimation } from '@/components/background-gradient-animation'
 import {
@@ -20,8 +22,27 @@ export default function Index(): React.ReactElement {
 		{ text: 'students', className: 'text-4xl text-foreground' }
 	]
 
+	const [isDarkMode, setIsDarkMode] = useState(false);
+
+	useEffect(() => {
+		const checkDarkMode = () => {
+			const isDark = document.documentElement.classList.contains('dark');
+			setIsDarkMode(isDark);
+		};
+
+		checkDarkMode();
+
+		const observer = new MutationObserver(checkDarkMode);
+
+		observer.observe(document.documentElement, { attributes: true });
+
+		return () => {
+			observer.disconnect();
+		};
+	}, []);
+
 	return (
-		<BackgroundGradientAnimation>
+		<BackgroundGradientAnimation gradientBackgroundStart={isDarkMode ? 'rgb(2,6,23)' : 'rgb(255,255,255)'} gradientBackgroundEnd={isDarkMode ? 'rgb(3,7,18)' : 'rgb(248,250,252)'} firstColor={isDarkMode ? '65,18,124' : '255,237,103'} secondColor={isDarkMode ? '65,18,124' : '255,237,103'} thirdColor={isDarkMode ? '65,18,124' : '255,237,103'} fourthColor={isDarkMode ? '65,18,124' : '255,237,103'} fifthColor={isDarkMode ? '65,18,124' : '255,237,103'} pointerColor={isDarkMode ? '65,18,124' : '255,237,103'}>
 			<div className=' flex flex-col justify-center items-center fixed inset-0 z-10'>
 				<div>
 					<TypewriterEffect
