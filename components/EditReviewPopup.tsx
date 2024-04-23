eab8cd848de2c055480bf895ace136d55bd1da61
Faizaan Qureshi <qfaizaan@gmail.com>
2024-04-23 19:53:00 -0400

    further normalize ui

--- components/EditReviewPopup.tsx
+++ components/EditReviewPopup.tsx
@@ -73,7 +73,7 @@ export default function EditReviewPopup({
     }
 
     return (
-        <div ref={popupRef} className="overflow-y-auto max-h-[90vh] border-2 border-slate-600 dark:border-slate-800 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-background dark:bg-background/80 backdrop-blur rounded-md max-w-xl w-11/12">
+        <div ref={popupRef} className="overflow-y-auto max-h-[90vh] border-2 border-secondary fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-background dark:bg-background/80 backdrop-blur rounded-md max-w-xl w-11/12">
             <form
                 className="animate-in flex-1 flex flex-col w-full justify-center dark:text-white"
                 action={() => submitEditReview(easy, useful, liked, instructor, text, courseName)}
