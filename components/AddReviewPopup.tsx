0cfffae529ce075fb910d0baf230eee14cccf7b2
Faizaan Qureshi <qfaizaan@gmail.com>
2024-04-23 19:52:23 -0400

    further normalize ui

--- components/AddReviewPopup.tsx
+++ components/AddReviewPopup.tsx
@@ -158,10 +158,10 @@ export default function AddReviewPopup({
                             <hr className="sm:hidden border-gray-300 dark:border-gray-700"></hr>
 
                             <div className="self-center sm:self-start inline-flex rounded-lg shadow-sm pt-4 sm:pb-0">
-                                <button onClick={() => setLiked(1)} type="button" className={`py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none ${liked ? 'bg-white dark:bg-slate-600' : 'bg-stone-300 dark:bg-slate-900'} dark:border-gray-700 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}>
+                                <button onClick={() => setLiked(1)} type="button" className={`py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none ${liked ? 'bg-white dark:bg-slate-600' : 'bg-neutral-400 dark:bg-slate-900'} dark:border-gray-700 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}>
                                     <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M7 10v12" /><path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z" /></svg>
                                 </button>
-                                <button onClick={() => setLiked(0)} type="button" className={`py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none ${!liked ? 'bg-white dark:bg-slate-600' : 'bg-stone-300 dark:bg-slate-900'} dark:border-gray-700 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}>
+                                <button onClick={() => setLiked(0)} type="button" className={`py-3 px-4 inline-flex items-center gap-x-2 -ms-px first:rounded-s-lg first:ms-0 last:rounded-e-lg text-sm font-medium focus:z-10 border border-gray-200 bg-white text-gray-800 shadow-sm disabled:opacity-50 disabled:pointer-events-none ${!liked ? 'bg-white dark:bg-slate-600' : 'bg-neutral-400 dark:bg-slate-900'} dark:border-gray-700 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600`}>
                                     <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 14V2" /><path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22h0a3.13 3.13 0 0 1-3-3.88Z" /></svg>
                                 </button>
                             </div>
@@ -177,7 +177,7 @@ export default function AddReviewPopup({
                     </div>
                 </div>
                 <div className='p-4 flex flex-row-reverse'>
-                    <button type="submit" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-base rounded-lg border border-transparent bg-secondary dark:text-white dark:hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                    <button type="submit" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-base rounded-lg border border-transparent bg-secondary dark:text-white disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
                         Submit
                     </button>
                 </div>
