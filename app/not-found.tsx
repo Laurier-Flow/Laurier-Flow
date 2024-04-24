7fbaccb1d30cba91594b32fba1ee4d4270f8f89f
Faizaan Qureshi <qfaizaan@gmail.com>
2024-04-24 08:42:21 -0400

    add standard 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { BackgroundGradientAnimation } from "@/components/background-gradient-animation";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <BackgroundGradientAnimation gradientBackgroundStart='var(--gradient-start)' gradientBackgroundEnd='var(--gradient-end)' firstColor='var(--bubble)' secondColor='var(--bubble)' thirdColor='var(--bubble)' fourthColor='var(--bubble)' fifthColor='var(--bubble)' pointerColor='var(--bubble)'>
+            <div className='flex flex-col justify-center items-center fixed inset-0 z-[100] p-6 max-w-6xl w-full mx-auto gap-12'>
+                <h1 className="text-background dark:text-foreground text-6xl font-bold">Feeling lost?</h1>
+                <h2 className="text-background dark:text-foreground text-xl font-semibold">The page you're trying to reach doesn't exist</h2>
+                <Link href="/">
+                    <h2 className="text-background dark:text-foreground text-xl font-semibold underline underline-offset-4">Return Home</h2>
+                </Link>
+                <h3 className="mt-8 text-slate-300 dark:text-foreground text-md font-semibold">404 - Not Found</h3>
+            </div>
+        </BackgroundGradientAnimation>
+    )
+}
