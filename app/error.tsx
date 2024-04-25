221bf6df446045961df9f6fdf8dd00d7e564c3ac
Faizaan Qureshi <qfaizaan@gmail.com>
2024-04-25 14:07:18 -0400

    fix hydration errors related to typeform component

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import FormWidget from "@/components/FormWidget";
 import { BackgroundGradientAnimation } from "@/components/background-gradient-animation";
 import Link from "next/link";
 
@@ -11,12 +12,13 @@ export default function NotFound() {
                 <h1 className="text-background dark:text-foreground text-3xl md:text-6xl font-bold">We ran into an error!</h1>
                 <h2 className="text-background dark:text-foreground text-lg md:text-xl font-semibold">This could be because you're trying to access a course that has not been offered recently/is no longer offered at Laurier</h2>
                 <h3 className="text-background dark:text-foreground md:text-lg font-base"><b>Why did this happen? Laurier Flow</b> only has access to courses offered in or after the Winter 2024 term. If a course you've accessed using our prerequisite/leads to tools brought you here, then the site is working properly. As more courses get offered in the future, our database will continue to get populated.</h3>
-                <h2 className="text-background dark:text-foreground md:text-xl font-semibold">If the reason above is NOT the reason you were brought here, then please report this bug/error and what we can do to replicate it. <b>Laurier Flow</b> is still in beta as of the current term, so 🪳 are expected.</h2>
+                <h2 className="text-background dark:text-foreground md:text-xl font-semibold">If the reason above is NOT the reason you were brought here, then please report this bug/error and what we can do to replicate it using the feedback button on the right. <b>Laurier Flow</b> is still in beta as of the current term, so 🪳 are expected.</h2>
                 <Link href="/">
                     <h2 className="text-background dark:text-foreground text-lg md:text-xl font-semibold underline underline-offset-4">Return Home</h2>
                 </Link>
                 <h3 className="mt-8 text-slate-300 dark:text-foreground text-md font-semibold">500 - Error</h3>
             </div>
+            <FormWidget />
         </BackgroundGradientAnimation>
     )
 }
