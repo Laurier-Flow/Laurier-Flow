5a337efe2254b05dadf2d58469fdadb50b2c3758
Muhammad Mujtaba <103689781+mhmmd-03@users.noreply.github.com>
2024-04-28 02:05:30 +0000

    Added the relevant fields in the UserNav Component

--- components/UserProfileNav.tsx
+++ components/UserProfileNav.tsx
@@ -14,49 +14,41 @@ import {
     DropdownMenuShortcut,
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
+import { signOut } from "@/utils/supabase/authActions"
 
   import { User } from "@supabase/supabase-js"
+import Link from "next/link"
   
-  export function UserNav({ user }: { user: User | null }) {
+  export function UserNav({ user }: { user: User}) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
-              {/* <AvatarImage src="/avatars/01.png" alt="@shadcn" /> */}
-              <AvatarFallback>{user?.email?.charAt(0)}</AvatarFallback>
+              <AvatarFallback>{user.user_metadata.first_name?.charAt(0)}{user.user_metadata.last_name?.charAt(0)}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">shadcn</p>
+              <p className="text-sm font-medium leading-none">{user.user_metadata.first_name} {user.user_metadata.last_name}</p>
               <p className="text-xs leading-none text-muted-foreground">
-                m@example.com
+                {user.email}
               </p>
             </div>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <DropdownMenuItem>
-              Profile
-              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+              <Link href={'/profile'}>
+                Profile
+              </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              Billing
-              <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              Settings
-              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-            </DropdownMenuItem>
-            <DropdownMenuItem>New Team</DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={signOut}>
             Log out
-            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
