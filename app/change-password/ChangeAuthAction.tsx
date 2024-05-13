4b1d98ac29ea6053a90c25e0bb6f27bff07bcaa3
Abdullah <abd.shahid247@gmail.com>
2024-05-12 21:34:16 -0400

    Added password view toggle for change-password

--- app/change-password/ChangeAuthAction.tsx
+++ app/change-password/ChangeAuthAction.tsx
@@ -14,30 +14,8 @@ export const handleChangePassword = async (newPassword: string) => {
     })
 
     if (error) {
-        return error.message
+        return { success: false, message: error.message}
     } else {
-        redirect('/')
-    }
-}
-
-export const authenticateWithTokens = async (accessToken: string, refreshToken: string) => {
-    const cookieStore = cookies();
-    const supabase = createClient(cookieStore);
-
-    const getSessionWithTokens = async () => {
-        if (accessToken && refreshToken) {
-            const { data, error } = await supabase.auth.setSession({
-                access_token: accessToken,
-                refresh_token: refreshToken,
-            });
-
-            if (error) {
-                throw new Error(`Error signing in: ${error.message}`);
-            }
-        };
-    }
-
-    if (accessToken && refreshToken) {
-        await getSessionWithTokens();
+        return { success: true, message: ''}
     }
 };
--- app/change-password/reset-password-func.tsx
+++ app/change-password/reset-password-func.tsx
@@ -1,34 +1,34 @@
-"use server";
-import { cookies } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { fetchUser } from "@/utils/supabase/authActions";
+// "use server";
+// import { cookies } from "next/headers";
+// import { createClient } from "@/utils/supabase/server";
+// import { fetchUser } from "@/utils/supabase/authActions";
 
-export const resetUserPassword = async (
-  new_password: string
-): Promise<boolean> => {
-  "use server";
+// export const resetUserPassword = async (
+//   new_password: string
+// ): Promise<boolean> => {
+//   "use server";
 
-  const user = await fetchUser();
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const { error } = await supabase.auth.updateUser({
-    email: user!.email!,
-    password: new_password,
-  });
-  if (error) {
-    console.log(error);
-    return false;
-  } else {
-    // Sign the user in again
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: user!.email!,
-      password: new_password,
-    });
-    if (error) {
-      console.log(error);
-      return false;
-    }
-    console.log(data);
-    return true;
-  }
-};
+//   const user = await fetchUser();
+//   const cookieStore = cookies();
+//   const supabase = createClient(cookieStore);
+//   const { error } = await supabase.auth.updateUser({
+//     email: user!.email!,
+//     password: new_password,
+//   });
+//   if (error) {
+//     console.log(error);
+//     return false;
+//   } else {
+//     // Sign the user in again
+//     const { data, error } = await supabase.auth.signInWithPassword({
+//       email: user!.email!,
+//       password: new_password,
+//     });
+//     if (error) {
+//       console.log(error);
+//       return false;
+//     }
+//     console.log(data);
+//     return true;
+//   }
+// };
