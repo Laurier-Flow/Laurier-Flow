d827f81a822001d8953598d670341b4ec86536fc
Abdullah <abd.shahid247@gmail.com>
2024-08-19 17:12:00 -0400

    Changed layout for analytics page

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -4,6 +4,7 @@ import { Suspense, useEffect, useState } from 'react'
 import Header from '@/components/Header'
 import { Metadata } from 'next'
 import Footer from '@/components/Footer'
+import { BackgroundGradientAnimation } from '@/components/background-gradient-animation'
 import {
 	getAllUsers,
 	getAllCourseReviews,
@@ -27,63 +28,19 @@ const CountPage = () => {
 	const width = useScreenWidth()
 	useEffect(() => {
 		const getData = async () => {
-			const date = new Date()
-
-			const daysOfWeek = [
-				'Sunday',
-				'Monday',
-				'Tuesday',
-				'Wednesday',
-				'Thursday',
-				'Friday',
-				'Saturday'
-			]
-			const months = [
-				'January',
-				'February',
-				'March',
-				'April',
-				'May',
-				'June',
-				'July',
-				'August',
-				'September',
-				'October',
-				'November',
-				'December'
-			]
-
-			const dayOfWeek = daysOfWeek[date.getDay()]
-			const month = months[date.getMonth()]
-			const dayOfMonth = date.getDate()
-			const year = date.getFullYear()
-
-			const formattedDate = `${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`
-
-			let hours = date.getHours()
-			const minutes = date.getMinutes()
-
-			const ampm = hours >= 12 ? 'pm' : 'am'
-
-			hours = hours % 12
-			hours = hours ? hours : 12
-
-			const formattedMinutes = minutes < 10 ? '0' + minutes : minutes
-
-			const formattedTime = `${hours}:${formattedMinutes}${ampm}`
-
-			setCurDate(formattedDate + ' @ ' + formattedTime)
 			const curUser = await getUser()
 			if (curUser) {
 				setUser(curUser)
 			} else {
 				setUser(null)
 			}
+
 			const allUsers = await getAllUsers()
 			const allCourseReviews = await getAllCourseReviews()
 			const allInstructorReviews = await getAllInstructorReviews()
 			const allCourses = await getAllCourses()
 			const totalVisits = await getAndIncrementPageVisits()
+
 			if (allUsers) {
 				setNumberUsers(allUsers)
 			}
@@ -113,166 +70,70 @@ const CountPage = () => {
 	}, [])
 
 	return (
-		<>
-			<Header user={user!} />
-			<Suspense
-				fallback={
-					<div className='h-full w-full'>
-						<Spinner />
-					</div>
-				}
-			>
-				<div className="flex min-w-full flex-col bg-[url('/banner-sm-light.jpg')] p-4 dark:bg-[url('/banner-sm.jpg')] md:flex-row md:justify-center md:bg-[url('/banner-md-light.jpg')] md:dark:bg-[url('/banner-md.jpg')] lg:bg-[url('/banner-light.jpg')] lg:dark:bg-[url('/banner.jpg')]">
-					<div className='w-f flex max-w-6xl flex-1 flex-row justify-between pt-20'>
-						<div className='flex flex-1 flex-col pl-4'>
-							<h1 className='mb-2 text-2xl font-bold text-white md:text-5xl'>
-								<span className='text-purple-200'>Laurier</span>{' '}
-								<span className='text-yellow-200'>Flow</span> Analytics
-								<br />
-								<span className='text-xl italic'>
-									Tracking since May 23, 2024
-								</span>
-							</h1>
-						</div>
-					</div>
-				</div>
-				<div
-					style={{ minHeight: '90vh', maxWidth: '80vw', minWidth: '80vw' }}
-					className=' mt-5'
-				>
-					<h1
-						style={{
-							borderRadius: 20
-							// marginTop: `${width < 1000 ? '0' : '-150px'}`
-						}}
-						className='mb-5 mt-10 bg-primary pb-5 pl-5 pr-5 pt-5 text-center text-2xl font-semibold text-white dark:bg-secondary'
-					>
-						As of {curDate}
-					</h1>
+		<BackgroundGradientAnimation
+			gradientBackgroundStart='var(--gradient-start)'
+			gradientBackgroundEnd='var(--gradient-end)'
+			firstColor='var(--bubble)'
+			secondColor='var(--bubble)'
+			thirdColor='var(--bubble)'
+			fourthColor='var(--bubble)'
+			fifthColor='var(--bubble)'
+			pointerColor='var(--bubble)'
+		>
+			<div className='absolute inset-0 z-[100] mx-auto flex w-full max-w-6xl flex-col items-center justify-center gap-6 p-6'>
+				<h1 className='text-3xl font-bold text-background dark:text-foreground md:text-4xl'>
+					Analytics
+				</h1>
 
-					<div
-						className='mb-5 mt-10 bg-secondary pb-5 pl-5 pr-5 pt-5 dark:bg-primary'
-						style={{
-							borderRadius: 20
-							// marginTop: `${width < 1000 ? '0' : '-150px'}`
-						}}
-					>
+				<div className={`grid grid-rows-2 mt-10 grid-cols-2 place-content-center gap-2`}>
+					<div className='mx-4 my-4'>
 						{fetched ? (
-							<h1 className='text-center text-2xl font-semibold text-white'>
-								{pageVisits} page visits to {width < 700 ? <br /> : null}
-								Laurier Flow
+							<h1 className='text-center text-2xl text-white'>
+								{pageVisits} site visits
 							</h1>
 						) : (
-							<Spinner />
+							<div className='mx-8 my-8'>
+								<Spinner />
+							</div>
 						)}
 					</div>
-					<div
-						style={
-							{
-								// marginTop: `${width < 1000 ? '0' : '-200px'}`
-							}
-						}
-						className={`grid ${width < 700 ? 'grid-rows-2' : 'grid-rows-2'} mt-10 grid-cols-2 place-content-center gap-2`}
-					>
-						<div
-							style={{
-								borderRadius: 20
-							}}
-							className='mb-2 w-full bg-primary pb-5 pl-5 pr-5 pt-5 text-white dark:bg-secondary'
-						>
-							<h3 className='font-regular text-center text-xl'>
-								Signed Up Users
-							</h3>
-							<hr className='mb-2 mt-2' />
-							{fetched ? (
-								<div>
-									<h3 className='font-regular text-center text-xl'>
-										{numberUsers}
-									</h3>
-								</div>
-							) : (
+					<div className='mx-4 my-4'>
+						{fetched ? (
+							<h1 className='text-center text-2xl text-white'>
+								{numberUsers} registered users
+							</h1>
+						) : (
+							<div className='mx-8 my-8'>
 								<Spinner />
-							)}
-						</div>
-						<div
-							style={{ borderRadius: 20 }}
-							className='mb-2 w-full bg-primary  pb-5 pl-5 pr-5 pt-5 text-white dark:bg-secondary'
-						>
-							<h3 className='font-regular text-center text-xl'>
-								Course Reviews
-							</h3>
-							<hr className='mb-2 mt-2' />
-							{fetched ? (
-								<div>
-									<h3 className='font-regular text-center text-xl'>
-										{numberCourseReviews}
-									</h3>
-								</div>
-							) : (
+							</div>
+						)}
+					</div>
+					<div className='mx-4 my-4'>
+						{fetched ? (
+							<h1 className='text-center text-2xl text-white'>
+								{numberCourseReviews} course reviews
+							</h1>
+						) : (
+							<div className='mx-8 my-8'>
 								<Spinner />
-							)}
-						</div>
-						<div
-							style={{ borderRadius: 20 }}
-							className='mb-2 w-full bg-primary pb-5 pl-5 pr-5 pt-5 text-white dark:bg-secondary'
-						>
-							<h3 className='font-regular text-center text-xl'>
-								Instructor Reviews
-							</h3>
-							<hr className='mb-2 mt-2' />
-							{fetched ? (
-								<div>
-									<h3 className='font-regular text-center text-xl'>
-										{numberInstructorReviews}
-									</h3>
-								</div>
-							) : (
+							</div>
+						)}
+					</div>
+					<div className='mx-4 my-4'>
+						{fetched ? (
+							<h1 className='text-center text-2xl text-white'>
+								{numberInstructorReviews} instructor reviews
+							</h1>
+						) : (
+							<div className='mx-8 my-8'>
 								<Spinner />
-							)}
-						</div>
-						<div
-							style={{ borderRadius: 20 }}
-							className='mb-2 w-full bg-primary pb-5 pl-5 pr-5 pt-5 text-white dark:bg-secondary'
-						>
-							<h3 className='font-regular text-center text-xl'>
-								Courses on Laurier Flow
-							</h3>
-							<hr className='mb-2 mt-2' />
-							{fetched ? (
-								<div>
-									<h3 className='font-regular text-center text-xl'>
-										{numberCourses}
-									</h3>
-								</div>
-							) : (
-								<Spinner />
-							)}
-						</div>
+							</div>
+						)}
 					</div>
 				</div>
-				{/* <div style={{ minHeight: '90vh' }} className='place-content-center'>
-					<div
-						style={{ borderRadius: 20 }}
-						className='mb-2 bg-primary pb-5 pl-5 pr-5 pt-5 text-white dark:bg-secondary'
-					>
-						<h1 className='text-center text-2xl font-semibold'>
-							Users that have signed up for LaurierFlow
-						</h1>
-						<hr className='mb-2 mt-2' />
-						{fetched ? (
-							<div>
-								<h1 className='text-center text-3xl font-semibold'>
-									{numberUsers}
-								</h1>
-							</div>
-						) : (
-							<Spinner />
-						)}
-					</div>
-				</div> */}
-			</Suspense>
-			<Footer />
-		</>
+				
+			</div>
+		</BackgroundGradientAnimation>
 	)
 }
 export default CountPage
